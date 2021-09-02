6cfbe8f97be31a79ee6aa51376cf736e9d209d1d
Pavel Paddubotski <pavel.paddubotski@gmail.com>
2021-09-02 21:17:06 +0300

    Custom Table View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Pavel Paddubotski on 2.09.21.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+}

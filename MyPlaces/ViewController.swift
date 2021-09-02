ee88c1de5a0ffd89b5e809b8734359a03938484e
Pavel Paddubotski <pavel.paddubotski@gmail.com>
2021-09-02 11:49:03 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Pavel Paddubotski on 2.09.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
